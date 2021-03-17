45757b949482aa1f009bbfa12dcbcf56dc93de7b
sanjana k <sanjanarthi@gmail.com>
2021-03-17 18:10:27 +0530

    Add project files.

---
+++ UserRegistrationProblem11/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistrationProblem11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ UserRegistrationProblem11/ValidateUserDetailsCustomException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserRegistrationProblem11
+{
+    class ValidateUserDetailsCustomException
+    {
+    }
+}
